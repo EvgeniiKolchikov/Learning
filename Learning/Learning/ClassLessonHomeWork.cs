83261f6f30aaa15fe4a6568c64d5ff99bc2827f8
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-08-30 18:58:12 +0300

    Домашнее задание по темам enum, конструктор, сет гет, switch

--- Learning/Learning/ClassLessonHomeWork.cs
+++ Learning/Learning/ClassLessonHomeWork.cs
@@ -3,39 +3,109 @@ namespace Learning;
 public class ClassLessonHomeWork
 {
     public string Name { get; set; }
-    public int AnimalType { get; set; }
 
-    public int Diagnosis{ get; set;}
+    private int _animalType;
+    public int AnimalType
+    {
+        set { AnimalSetCheck(value); }
+        get { return _animalType; }
+    }
 
-    public ClassLessonHomeWork(string name, AnimalTypeEnum animalType, DiagnosisEnum diagnosis = DiagnosisEnum.ItWillPass)
+    private int _diagnosis;
+    public int Diagnosis
+    {
+        set { DiagnosisSetCheck(value); }
+        get { return _diagnosis; }
+    }
+    
+    private int _initialInspectionCost = 0;
+    private int _totalCost = 0;
+    
+    public ClassLessonHomeWork(string name, int animalType, int diagnosis = (int)DiagnosisEnum.NoHelp)
     {
         Name = name;
-        AnimalType = (int)animalType;
-        Diagnosis = (int)diagnosis;
+        AnimalSetCheck(animalType);
+        DiagnosisSetCheck(diagnosis);
     }
-
     public enum AnimalTypeEnum
     {
-        Cat = 1,
-        Dog = 2,
-        Cavy = 3,
-        Parrot = 4
+        Cat ,
+        Dog ,
+        Cavy ,
+        Parrot
     }
-
     public enum DiagnosisEnum
     {
-        Medicate = 1,
-        Surgery = 2,
-        ItWillPass = 3
+        Medicate ,
+        Surgery ,
+        NoHelp
     }
 
     public void AddDiagnosis(DiagnosisEnum diagnosis)
     {
         Diagnosis = (int)diagnosis;
+        TotalCostCalculate();
+    }
+    public override string ToString()
+    {
+        InitialCostCheck();
+        TotalCostCalculate();
+        return $"{Name}, {(AnimalTypeEnum)AnimalType}, {((DiagnosisEnum)Diagnosis)}, {_totalCost.ToString()}";
     }
 
-    public void CalculateCostService()
+    private void AnimalSetCheck(int value)
     {
+        if (value >= 0 && value < Enum.GetNames(typeof(AnimalTypeEnum)).Length)
+        {
+            _animalType = value;
+            return;
+        }
+
+        throw new ArgumentException(
+            $"Введите значение в диапазоне от 0 до {Enum.GetNames(typeof(AnimalTypeEnum)).Length}");
+    }
+    private void DiagnosisSetCheck(int value)
+    {
+        if (value >= 0 && value < Enum.GetNames(typeof(DiagnosisEnum)).Length)
+        {
+            _diagnosis = value;
+            return;
+        }
+        throw new ArgumentException(
+            $"Введите значение в диапазоне от 0 до {Enum.GetNames(typeof(DiagnosisEnum)).Length}");
+    }
+    private void InitialCostCheck()
+    {
+        switch (AnimalType)
+        {
+            case (int)AnimalTypeEnum.Dog:
+                _initialInspectionCost = 1000;
+                break;
+            case (int)AnimalTypeEnum.Cat:
+                _initialInspectionCost = 800;
+                break;
+            case (int)AnimalTypeEnum.Cavy:
+                _initialInspectionCost = 200;
+                break;
+            case (int)AnimalTypeEnum.Parrot:
+                _initialInspectionCost = 500;
+                break;
+        }
+    }
+    private void TotalCostCalculate()
+    {
+        switch (Diagnosis)
+        {
+            case (int)DiagnosisEnum.Medicate:
+                _totalCost = _initialInspectionCost * 2;
+                break;
+            case (int)DiagnosisEnum.Surgery:
+                _totalCost = _initialInspectionCost * 5;
+                break;
+            case (int)DiagnosisEnum.NoHelp:
+                _totalCost = _initialInspectionCost;
+                break;
+        }
         
     }
 }
--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -1,5 +1,14 @@
 ﻿using Learning;
 
 
-var bobik = new ClassLessonHomeWork("Bobik",ClassLessonHomeWork.AnimalTypeEnum.Cat);
-bobik.AddDiagnosis(ClassLessonHomeWork.DiagnosisEnum.Surgery);
+var bobik = new ClassLessonHomeWork("Bobik",0);
+
+var barsik = new ClassLessonHomeWork("Barsik", 3, 1);
+
+bobik.AddDiagnosis(ClassLessonHomeWork.DiagnosisEnum.NoHelp);
+barsik.AddDiagnosis(ClassLessonHomeWork.DiagnosisEnum.Surgery);
+
+Console.WriteLine(bobik.ToString());
+Console.WriteLine(barsik.ToString());
+
+Console.Read();
