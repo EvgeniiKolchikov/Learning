6ee32ce6382a0df8cb1b0dd79071d640db2e8cf5
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-09-01 16:47:15 +0300

    Пример рекурсивного метода

--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -1,7 +1,14 @@
 ﻿using Learning;
 using MyLibrary;
 
-var bobik = new ClassLessonHomeWork("Bobik", 1);
-bobik.AddDiagnosis(ClassLessonHomeWork.DiagnosisEnum.Surgery);
-Console.WriteLine(bobik.ToString());
+int Recursion(int a, int b)
+{
+    if (a < 0 || b < 0)  throw new Exception("а и b должны быть больше нуля");
+    if (a == 0)  return b;
+    if (b == 0) return a;
+
+    return Recursion(a + 1, b - 1);
+}
+
+Console.WriteLine(Recursion(87,5).ToString());
 
