8799282f1d1db3f6e257f2030cbb22d86e71e9e2
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-09-01 16:46:34 +0300

    Урок модификаторы доступа

--- Learning/MyLibrary/Lib.cs
+++ Learning/MyLibrary/Lib.cs
@@ -3,4 +3,7 @@
 public class Lib
 {
     public int Number { get; set; }
+    private protected int Mmm;
+    protected string ProtectedString = "Protected";
+    internal protected string ProtectedInternal = "ProtectedInternal";
 }
