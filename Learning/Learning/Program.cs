27407df136e477efdb91e84bff39954dfd7347bb
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-09-01 21:42:13 +0300

    Пример рекурсивного метода: Найти наибольшее значение

--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -1,14 +1,14 @@
 ﻿using Learning;
 using MyLibrary;
 
+var count = 0;
 int Recursion(int a, int b)
 {
     if (a < 0 || b < 0)  throw new Exception("а и b должны быть больше нуля");
-    if (a == 0)  return b;
-    if (b == 0) return a;
-
-    return Recursion(a + 1, b - 1);
+    if (a == 0)  return b + count;
+    if (b == 0) return a + count;
+    count++;
+    return Recursion(a - 1, b - 1);
 }
-
-Console.WriteLine(Recursion(87,5).ToString());
+Console.WriteLine(Recursion(8,5).ToString());
 
