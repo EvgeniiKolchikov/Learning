d96e7f16c49dc27ebeb4d2a0db2ac7a411cb610b
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-09-08 21:01:07 +0300

    Урок по override и скрытие методов

---
+++ Learning/Learning/OverrideVirtualLesson.cs
@@ -0,0 +1,133 @@
+namespace Learning;
+
+public class OverrideVirtualLesson
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        private int _age = 1;
+
+        public virtual int Age
+        {
+            get => _age;
+            set
+            {
+                if (value > 0 && value < 110) _age = value;
+            }
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    public class Employee : OverrideVirtualLesson.Person
+    {
+        public string Company { get; set; }
+
+        public override int Age
+        {
+            get => base.Age;
+            set
+            {
+                if (value > 17 && value < 65)
+                {
+                    base.Age = value;
+                }
+            }
+        }
+
+        public Employee(string name, string company) : base(name)
+        {
+            Company = company;
+            base.Age = 18;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"{Name} работает в {Company}");
+        }
+
+    }
+
+
+
+    public class HidingLesson
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public Person(string name)
+            {
+                Name = name;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine(Name);
+            }
+        }
+
+        public class Employee : HidingLesson.Person
+        {
+            public string Company { get; set; }
+
+            public Employee(string name, string company) : base(name)
+            {
+                Company = company;
+
+            }
+
+            public new void Print()
+            {
+                Console.WriteLine($"{Name} работает в {Company}");
+            }
+        }
+    }
+
+
+
+    public abstract class Shape
+    {
+        public virtual void GetPerimeter()
+        {
+            Console.WriteLine("Perimeter");
+        }
+
+        public virtual void GetArea()
+        {
+            Console.WriteLine("Area");
+        }
+    }
+
+    public class Square : Shape
+    {
+        private int Side { get; set; }
+
+        public Square(int side)
+        {
+            Side = side;
+        }
+
+        public override void GetPerimeter()
+        {
+            Console.WriteLine($"Периметр равен  {Side * 4}");
+        }
+
+        public override void GetArea()
+        {
+            base.GetArea();
+            Console.WriteLine($"Площадь равна {Side * Side}");
+        }
+
+    }
+
+}
--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -1,39 +1,13 @@
 ﻿using System.Text.Json.Nodes;
 using Learning;
 
-//Восходящие преобразования. Upcasting. Базовому классу приводим класс наследника
-Employee employee = new Employee("Tom","Microsoft");
-Person person = employee; //Ссылка на один объект в куче, только person доступен его функционал.
+OverrideVirtualLesson.HidingLesson.Person person = new OverrideVirtualLesson.HidingLesson.Person("Tom");
+person.Print();
 
-Person bob = new Client("Bob", "SberBank");// Также восходящее преобразование. bob доступен только функционал класса Person
+OverrideVirtualLesson.HidingLesson.Person employee = new OverrideVirtualLesson.HidingLesson.Employee("Ken", "Microsoft");
+employee.Print();
 
-object person1 = new Person("Person1");//у person1 функционал только класса Object.
-object person2 = new Employee("Person2", "Google");// person2 не имеет полей и методов класса Employee
+OverrideVirtualLesson.Person p = new OverrideVirtualLesson.Employee("Hi","YU");
+p.Print();
 
-//Нисходящие преобразования. Downcasting. 
-
-Employee employee1 = new Employee("Greg", "HP");
-Person person3 = employee1; // Upcasting 
-Employee employee2 = (Employee)person3; // явно привели person к employee. 
-
-object obj = new Employee("Hary", "Apple");
-((Person)obj).Print();
-((Employee)obj).Print();
-var company = ((Employee)obj).Company;
-
-object obj1 = new Employee("Tim", "Kkk");
-//var bank = ((Client)obj1).Bank; // ошибка выполнения. Нельзя привести тип Client к Employee. Так как они не наследуются друг от друга
-
-//Employee employee3 = new Person("Nil"); // ошибка мы пытаемся преобразовать объект типа Person к типу Employee.а объект Person не является объектом Employee
-
-Person person4 = new Person("Max");
-//Employee employee3 = (Employee)person4; 
-Employee? employee3 = person4 as Employee;
-if(employee3 == null) Console.WriteLine("неудачно");
-else Console.WriteLine(employee3.Company);
-
-if (person4 is Employee employee5)
-{
-    Console.WriteLine(employee5.Company);
-}
-else Console.WriteLine("преобразование недопустимо");
+Console.Read();
