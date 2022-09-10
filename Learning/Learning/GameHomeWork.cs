365fd63060d88217c6306d3c8b2ebb99dfaa06f1
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-09-10 21:46:37 +0300

    Домашнее задание по Главе 4. Игра выполнена!

--- Learning/Learning/GameHomeWork.cs
+++ Learning/Learning/GameHomeWork.cs
@@ -5,7 +5,7 @@ public class GameHomeWork { }
 public abstract class Unit
 {
     protected int AttackPower { get; set; }
-    protected int Health { get; set; }
+    public int Health { get; set; }
 
     public Unit(int health, int attackPower)
     {
@@ -46,6 +46,7 @@ public class Warrior : Unit
     {
         Health -= damage / 2;
         Console.WriteLine($"Воин получил урон {damage/2}");
+        Console.WriteLine($"Здоровье воина {Health}");
         return Health > 0;
     }
 }
@@ -56,7 +57,7 @@ public class Slayer : Unit
 
     public override int Attack()
     {
-        var rnd = new Random().Next(1,3);
+        var rnd = new Random().Next(1,4);
         Console.WriteLine($"Убийца нанес удар с силой {AttackPower * rnd}");
         return AttackPower * rnd;
     }
@@ -66,6 +67,7 @@ public class Slayer : Unit
         var rnd = new Random().Next(1,5);
         Health -= damage / rnd;
         Console.WriteLine($"Убийца получил урон с силой {damage / rnd}");
+        Console.WriteLine($"Здоровье убийцы {Health}");
         return Health > 0;
     }
 }
@@ -76,7 +78,7 @@ public class Archer : Unit
 
     public override int Attack()
     {
-        var rnd = new Random().Next(1, 3); 
+        var rnd = new Random().Next(1, 4); 
         Console.WriteLine($"Лучник нанес удар с силой {AttackPower * rnd}");
         return AttackPower * rnd;
     }
@@ -86,6 +88,114 @@ public class Archer : Unit
         var rnd = new Random().Next(1, 4);
         Health -= damage / rnd;
         Console.WriteLine($"Лучник получил урон {damage/rnd}");
+        Console.WriteLine($"Здоровье лучника {Health}");
         return Health > 0;
     }
 }
+
+public enum WeaponTypeEnum { Axe = 1, Dagger = 2 }
+public class Player : Unit
+{
+    private int _weaponType;
+    private int WeaponType
+    {
+        
+        set
+        {
+            if (value >= 1 || value <= 2)
+            {
+                _weaponType = value;
+                return;
+            }
+            value = 1;
+            _weaponType = value;
+        }
+    }
+    public Player(int health, int attackPower, WeaponTypeEnum weaponType) : base(health, attackPower)
+    {
+        _weaponType = (int)weaponType;
+    }
+
+    public override int Attack()
+    {
+        if (_weaponType == (int)WeaponTypeEnum.Axe)
+        {
+            var rnd = new Random().Next(1, 3);
+            Console.WriteLine($"Игрок нанес удар топором с силой {AttackPower * rnd}");
+            return AttackPower * rnd;
+        }
+
+        var rand = new Random().Next(1, 11);
+        Console.WriteLine($"Игрок нанес удар кинжалом с силой {AttackPower / 2 * rand}");
+        return AttackPower / 2 * rand;
+    }
+
+    public override bool TakeDamage(int damage)
+    {
+        var rnd = new Random().Next(1, 3);
+        Health -= damage/rnd;
+        Console.WriteLine($"Игрок получил урон {damage/rnd}");
+        Console.WriteLine($"Здоровье игрока: {Health}");
+        return Health > 0;
+    }
+}
+
+public class BattleField
+{
+    private Unit[] _enemies;
+    private Player _player;
+
+    public BattleField(Unit[] enemies, Player player)
+    {
+        foreach (var enemy in enemies)
+        {
+            if (enemy is not Warrior && enemy is not Slayer && enemy is not Archer)
+            {
+                throw new ArgumentException("Необходим массив врагов");
+            }
+        }
+        _enemies = enemies;
+        _player = player;
+    }
+
+    public void Fight()
+    {
+        var playerAlive = true;
+        var enemyAlive = true;
+        while (playerAlive && _enemies.Length != 0)
+        {
+            var rnd = new Random().Next(0, _enemies.Length);
+            
+            Console.Read();
+            var damage = _enemies[rnd].Attack();
+            playerAlive = _player.TakeDamage(damage);
+            if (!playerAlive) { break;}
+            
+            var playerDamage = _player.Attack();
+            enemyAlive = _enemies[rnd].TakeDamage(playerDamage);
+            
+            if (enemyAlive is false)
+            {
+                Console.WriteLine($"{_enemies[rnd]} погиб");
+                DeleteEnemyFromEnemies(ref _enemies,rnd);
+            }
+            Console.Read();
+        }
+
+        if (playerAlive == false) { Console.WriteLine("########### Игрок Погиб ############"); }
+
+        if (_enemies.Length == 0){ Console.WriteLine("########### Игрок Победил ############"); }
+        
+        
+    }
+
+    private void DeleteEnemyFromEnemies(ref Unit[] enemies, int index)
+    {
+        var newArray = new Unit[enemies.Length - 1];
+        enemies[index] = null!;
+        var temp =  enemies.Where(x => x != null).ToArray();
+        Array.Copy(temp,newArray,newArray.Length);
+        enemies = newArray;
+    }
+}
+
--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -3,12 +3,19 @@ using Learning;
 
 
 
-var warrior = new Warrior(120, 35);
+var warrior = new Warrior(120, 30);
 var slayer = new Slayer(80, 24);
-var archer = new Archer(50, 30);
+var archer = new Archer(50, 12);
+var player = new Player(250, 29, WeaponTypeEnum.Axe);
 
-var units = new Unit[] { warrior, slayer, archer };
-foreach (var unit in units)
-{
-    unit.Attack();
-}
+var enemies = new Unit[] { warrior, slayer, archer };
+
+var battle = new BattleField(enemies, player);
+
+Console.WriteLine("Press Enter To Next Step");
+battle.Fight();
+
+Console.Read();
+
+
+
