294460800048d517738f79bb555254223d29c3b2
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-09-14 21:03:22 +0300

    Урок Делегат

--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -1,31 +1,12 @@
-﻿using System.Text.Json.Nodes;
-using Learning;
+﻿using Learning;
 
 
 
-var tryCatch = new TryCatchLesson();
-// tryCatch.DivideToZeroWithTryCatch();
-// tryCatch.Square("43");
-// tryCatch.Square("f");
-// tryCatch.Divide();
-// tryCatch.ExceptionFilter();
-// tryCatch.CatchBlocks();
-//tryCatch.ThrowMethod();
+Message mes;
 
-// try
-// {
-//     var person = new EPerson { Name = "Tom", Age = 17 };
-// }
-// catch(PersonException e)
-// {
-//     Console.WriteLine($"Ошибка: {e.Message}");
-//     Console.WriteLine($"Некорректное значение: {e.Value}");
-//     throw;
-// }
+mes = Hello;
+mes();
 
-// var catchException = new CatchExceptionFindingLesson();
-// catchException.Main();
+void Hello() => Console.WriteLine("Helllo Woorld");
 
-
-
-
+delegate void Message();
