c2e3f31ec1588ff373f31634280bfc4ac8f7323d
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-10-04 17:03:55 +0300

    Домашнее задание по Regex и StringBuilder

--- Learning/Learning.HomeWorks/Program.cs
+++ Learning/Learning.HomeWorks/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Learning.HomeWorks;
 
-Console.WriteLine("Hello, World!");
+
+var archive = new []
+{
+    "AA134AA77", "AA111AA77", "BG543JK37", "AA113BH78", "GF456JK03", "HA143KL65"
+};
+
+
+var input = Console.ReadLine();
+
+var regex = StringBuilderRegexHomeWork.RegexInit(input);
+
+var matches = StringBuilderRegexHomeWork.GetMatches(regex, archive);
+
+StringBuilderRegexHomeWork.ShowMatches(matches);
+
+
+
+
+
---
+++ Learning/Learning.HomeWorks/StringBuilderRegexHomeWork.cs
@@ -0,0 +1,62 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Learning.HomeWorks;
+
+public class StringBuilderRegexHomeWork
+{
+    public static Regex RegexInit(string input)
+    {
+        if (input.Length == 0)
+        {
+            Console.WriteLine("Пусто, Введите значение поиска");
+            throw new ArgumentNullException();
+        }
+        
+        var sb = new StringBuilder();
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (Char.IsLetterOrDigit(input[i]))
+            {
+                sb.Append(input[i]);
+                continue;
+            }
+            if (input[i] == '*')
+            {
+                sb.Append(@"\w");
+                continue;
+            }
+            throw new ArgumentException("Некорректный формат поиска");
+        }
+        return new Regex(@"^" + sb + @"\w*" , RegexOptions.IgnoreCase);
+    }
+
+    public static IEnumerable<string> GetMatches(Regex regex, string[] archive)
+    {
+        for (var i = 0; i < archive.Length; i++)
+        {
+            if (Regex.IsMatch(archive[i],regex.ToString(),RegexOptions.IgnoreCase))
+            {
+                yield return archive[i];
+            }
+        }
+    }
+
+    public static void ShowMatches(IEnumerable<string> collection)
+    {
+        if (collection.Count() > 0)
+        {
+            foreach (var match in collection)
+            {
+                Console.WriteLine(match);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Совпадений не найдено");
+        }
+    }
+    
+
+
+}
