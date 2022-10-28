6e69b165eae6ecd3db0a849be2c3445f6755cdc3
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-10-28 20:22:44 +0300

    Урок Asyn/Await

---
+++ Learning/Learning/AsyncAwaitLesson.cs
@@ -0,0 +1,37 @@
+namespace Learning;
+
+public class AsyncAwaitLesson
+{
+   
+    public async Task ShowMessage()
+    {
+        Console.WriteLine(DateTime.Now);
+        
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"First methd - {i} element ");
+                Thread.Sleep(100);
+            }
+        });
+        Console.WriteLine("Первый метод закончился " + DateTime.Now);
+    }
+
+    public async Task ShowMessage2()
+    {
+        
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Second methd - {i} element ");
+                Thread.Sleep(100);
+            }
+        });
+        Console.WriteLine("Второй метод закончился: " + DateTime.Now);
+    }
+}
+
+
+// Если тип возвращаемого значения метода void то мы не можем использовать await для методов void,
