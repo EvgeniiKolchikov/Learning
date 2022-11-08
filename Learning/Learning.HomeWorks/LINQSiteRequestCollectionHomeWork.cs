af33381e5de7359f105662f920b11f45dc4b08ff
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-11-08 22:34:13 +0300

    Домашнее задание LINQ по коллекции Ping

---
+++ Learning/Learning.HomeWorks/LINQSiteRequestCollectionHomeWork.cs
@@ -0,0 +1,86 @@
+using System.Net.NetworkInformation;
+
+namespace Learning.HomeWorks;
+
+public class LINQHomeWork
+{
+    public List<MyPing> MyList { get; }
+    public LINQHomeWork()
+    {
+        MyList = new List<MyPing>();
+    }
+
+    public async Task Run()
+    {
+        
+        
+    }
+    
+    public async Task PingSitesAsync(IEnumerable<string> addresses)
+    {
+        foreach (var address in addresses)
+        {
+            var myPing = await SiteRequestAsync(address);
+            SendToCollection(myPing);
+        }
+    }
+    private void SendToCollection(MyPing myPing)
+    {
+        lock (MyList)
+        {
+            MyList.Add(myPing);
+        }
+    }
+    private async Task<MyPing> SiteRequestAsync(string address)
+    {
+        return await Task.Run(() => SiteRequest(address));
+    }
+    private MyPing SiteRequest(string address)
+    {
+        var ping = new Ping();
+        var myPing = new MyPing();
+        myPing.Address = address;
+        try
+        {
+            var reply = ping.Send(address,1000);
+            if (reply.Status == IPStatus.Success)
+            {
+                myPing.AddressExist = true;
+                return myPing;
+            }
+        }
+        catch (Exception e) 
+        {
+            Console.WriteLine(e.Message);
+        }
+        myPing.AddressExist = false;
+        return myPing;
+    }
+}
+
+public class MyPing
+{
+    public string Address { get; set; }
+    public bool AddressExist { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Address} - {AddressExist}";
+    }
+}
+
+public class NewPing
+{
+    public string Address { get; set; }
+    public bool AddressExist { get; set; }
+    public DateTime MyDateTime;
+    
+}
+
+
+
+
+
+
+
+
--- Learning/Learning.HomeWorks/Program.cs
+++ Learning/Learning.HomeWorks/Program.cs
@@ -1,111 +1,20 @@
-﻿using System.Text;
+﻿
 using Learning.HomeWorks;
-var asyncWriter = new AsyncWriter();
-await asyncWriter.WriteNumber();
- 
-public class AsyncWriter
+
+var sites = new []{"google.com", "metanit.ru"};
+
+var linqHW = new LINQHomeWork();
+
+await linqHW.PingSitesAsync(sites);
+
+var myList = linqHW.MyList;
+
+var linq1 = myList.Select(p => new NewPing
 {
-    object _numberRuningThread = 0;
-    object _laseWriteNumber = 3;
-    public static bool JobIsDone = false;
-    public readonly int[] NumberArr;
- 
-    public AsyncWriter()
-    {
-        NumberArr = new int[30];
-        var rnd = new Random();
-        for (int i = 0; i < NumberArr.Length; i++)
-        {
-            NumberArr[i] = rnd.Next(1, 4);
-        }
-    }
- 
-    public async Task WriteNumber()
-    {
-        for (int i = 1; i <= 3; i++)
-        {
-            Thread myThread = new(RunWriter);
-            myThread.Start();
-        }
-        await CheckIsEnd();
-    }
- 
-    private void RunWriter()
-    {
-        _numberRuningThread = (int)_numberRuningThread + 1;
-        var writeNumber = (int)_numberRuningThread;
-        var index = 0;
-        while (index < NumberArr.Length)
-        {
-            if (writeNumber == 1 && (int)_laseWriteNumber == 3)
-            {
-                if (NumberArr[index] == writeNumber)
-                {
-                    Console.Write(NumberArr[index]);
-                    _laseWriteNumber = 1;
-                    index++;
-                }
-                else
-                {
-                    index++;
-                }
-            }else if (writeNumber == 2 && (int)_laseWriteNumber == 1)
-            {
-                if (NumberArr[index] == writeNumber)
-                {
-                    Console.Write(NumberArr[index]);
-                    _laseWriteNumber = 2;
-                }
-                else
-                {
-                    index++;
-                }
-            }else if (writeNumber == 3 && (int)_laseWriteNumber == 2)
-            {
-                if (NumberArr[index] == writeNumber)
-                {
-                    Console.Write(NumberArr[index]);
-                    _laseWriteNumber = 3;
-                }
-                else
-                {
-                    index++;
-                }
-            }
-            else
-            {
-                Thread.Sleep(100);
-            }
-        }
+    Address = p.Address,
+    AddressExist = p.AddressExist,
+    MyDateTime = DateTime.Now
+});
 
-        while (index < NumberArr.Length)
-        {
-            if (writeNumber == 2 && (int)_laseWriteNumber == 3)
-            {
-                if (NumberArr[index] == writeNumber)
-                {
-                    Console.Write(NumberArr[index]);
-                    _laseWriteNumber = 2;
-                    index++;
-                }
-                else
-                {
-                    index++;
-                }
-            }
-        }
-        
-        return;
-    }
- 
-    private async Task CheckIsEnd()
-    {
-        while (!JobIsDone)
-        {
-            Thread.Sleep(100);
-        }
-        return;
-    }
-}
-
-
+foreach (var el in linq1)
+    Console.WriteLine($"{el.Address} - {el.AddressExist} - {el.MyDateTime}");
