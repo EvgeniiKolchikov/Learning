54402717f8f0031d629f439b318efbaa3f9c94ae
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-11-12 22:43:33 +0300

    Урок Рефлексия

--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -1,21 +1,9 @@
 ﻿using Learning;
 
-var linqLesson = new LINQLesson();
-// linqLesson.Linq1();
-// linqLesson.Linq2();
-// linqLesson.Linq3();
-// linqLesson.Linq4();
-//linqLesson.Linq5();
-// linqLesson.Linq6();    
-// linqLesson.Linq7();
-// linqLesson.Linq8();
-// linqLesson.Linq9();
-// linqLesson.Linq10();
-// linqLesson.Linq11();
-// linqLesson.Linq12();
-// linqLesson.Linq13();
-//linqLesson.Linq14();
-// linqLesson.Linq15();
-// linqLesson.Linq16();
-linqLesson.Linq17();
+var reflection = new ReflectionLesson();
+//reflection.Typeof_Method();
+//reflection.GetMembers_Method();
+//reflection.GetMethods_Method();
+//reflection.GetParameters_Method();
+reflection.InvokeMethod_Method();
 
---
+++ Learning/Learning/ReflectionLesson.cs
@@ -0,0 +1,176 @@
+using System.Reflection;
+
+namespace Learning;
+
+public class ReflectionLesson
+{
+    public void Typeof_Method()
+    {
+        var myTipe = typeof(RPerson);
+        Console.WriteLine("typeof " + myTipe);
+
+        // RPerson pers = new RPerson("N");
+        // Type myTipe2 = pers.GetType();
+        // Console.WriteLine("GetType Method " + myTipe2);
+        //
+        // Type? myTipe3 = Type.GetType("Learning.RPerson",false,true);
+        // Console.WriteLine("Type.GetType " + myTipe3);
+
+        Console.WriteLine($"Name: {myTipe.Name}");
+        Console.WriteLine($"FullName: {myTipe.FullName}");
+        Console.WriteLine($"NameSpace: {myTipe.Namespace}");
+        Console.WriteLine($"Is Value Type: {myTipe.IsValueType}");
+        Console.WriteLine($"IS class: {myTipe.IsClass}");
+
+        Console.WriteLine("\nРеализованные интерфейсы");
+        foreach (var interf in myTipe.GetInterfaces()) Console.WriteLine(interf);
+    }
+
+    public void GetMembers_Method()
+    {
+        var myType = typeof(RPerson2);
+
+        foreach (var member in myType.GetMembers())
+            Console.WriteLine($"{member.DeclaringType}, {member.MemberType}, {member.Name}");
+        {
+            Console.WriteLine(new string('#', 50));
+        }
+        foreach (var member in myType.GetMembers(BindingFlags.DeclaredOnly
+                                                 | BindingFlags.Instance | BindingFlags.NonPublic |
+                                                 BindingFlags.Public))
+        {
+            Console.WriteLine($"{member.DeclaringType}, {member.MemberType}, {member.Name}");
+        }
+    }
+    public void GetMethods_Method()
+    {
+        var myType = typeof(PrinterR);
+
+        Console.WriteLine("Методы: ");
+
+        foreach (var method in myType.GetMethods()
+                     .Where(m => !m.Name.StartsWith("get_") && !m.Name.StartsWith("set_")))
+        {
+            var modificator = "";
+
+            if (method.IsStatic) modificator += "static ";
+            if (method.IsVirtual) modificator += "virtual ";
+
+            Console.WriteLine($"{modificator}{method.ReturnType.Name},{method.Name}");
+        }
+
+        Console.WriteLine(new string('#', 50));
+
+        foreach (var method in myType.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance
+                                                                           | BindingFlags.NonPublic |
+                                                                           BindingFlags.Public))
+            Console.WriteLine($"{method.ReturnType} {method.Name}");
+    }
+
+    public void GetParameters_Method()
+    {
+        var myType = typeof(PrinterR);
+        foreach (MethodInfo method in myType.GetMethods())
+        {
+            Console.Write($"Метод: {method.ReturnType} {method.Name} (");
+
+            ParameterInfo[] parameters = method.GetParameters();
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                var param = parameters[i];
+
+                var accessModificator = "";
+
+                if (param.IsIn) accessModificator += "in";
+                else if (param.IsOut) accessModificator += "out";   
+                
+                Console.Write($"Параметр: {param.ParameterType.Name} {accessModificator} {param.Name}");
+                if (param.HasDefaultValue) Console.Write($" = {param.DefaultValue}");
+
+                if (i < parameters.Length) Console.Write(",");
+            }
+
+            Console.WriteLine(")");
+
+        }
+    }
+
+    public void InvokeMethod_Method()
+    {
+        var printer = new PrinterInvoke("Inner Peace");
+        var printMethod = typeof(PrinterInvoke).GetMethod("Print", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+        printMethod?.Invoke(printer, parameters: null);
+
+    }
+}
+
+public class PrinterInvoke
+{
+    public string Text { get;}
+    public PrinterInvoke(string text) => Text = text;
+    private void Print() => Console.WriteLine(Text);
+}
+
+internal class PrinterR
+{
+    public string DefaultMessage { get; set; } = "Hello";
+
+    public void PrintMessage(string message, int times = 1)
+    {
+        while (times-- > 0) Console.WriteLine(message);
+    }
+
+    public string CreateMessage()
+    {
+        return DefaultMessage;
+    }
+}
+
+public class RPerson : IEater, IMovableR
+{
+    public string Name { get; }
+
+    public RPerson(string name)
+    {
+        Name = name;
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine($"{Name} eat");
+    }
+
+    public void Move()
+    {
+        Console.WriteLine($"{Name} moves");
+    }
+}
+
+internal interface IEater
+{
+    void Eat();
+}
+
+internal interface IMovableR
+{
+    void Move();
+}
+
+public class RPerson2
+{
+    private readonly string name;
+    public int Age { get; set; }
+
+    public RPerson2(string name, int age)
+    {
+        this.name = name;
+        Age = age;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Name: {name}, Age: {Age}");
+    }
+}
