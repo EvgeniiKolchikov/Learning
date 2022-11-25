8112a9a0c7988d430bf6ffdda9f2fb985bd72ceb
Evgenii Kolchikov <evgeniy_kolchikov@mail.ru>
2022-11-25 14:52:04 +0300

    Урок JSON

--- Learning/Learning/JSONLesson.cs
+++ Learning/Learning/JSONLesson.cs
@@ -10,6 +10,11 @@ public class PersonJSON
     public string Name { get; set; }
     public int Age { get; set; }
 
+    public PersonJSON()
+    {
+        
+    }
+
     public PersonJSON(string name, int age)
     {
         Name = name;
